6f7a7685c73867438fb51ab0f5dbcb064159c8e3
Guy Barros <38210175+GuyBarros@users.noreply.github.com>
2018-12-05 21:39:46 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "algorithm" {
+  description = "The Algorithm used to create the private key"
+  default     = "ECDSA"
+}
+
+variable "ecdsa_curve" {
+  description = "The ecdsa curve used to create the private key"
+  default     = "P521"
+}
+
+variable "common_name" {
+  description = "Common name for the TLS certificate"
+  default     = "service.consul"
+}
+
+variable "organization" {
+  description = "Organization the TLS certificate"
+  default     = "HashiCorp Demostack"
+}
+
+variable "validity_period_hours" {
+  description = "Validity period of the TLS certificate"
+  default     = 720
+}
+
+variable "is_ca_certificate" {
+  description = "will this TLS certificate be a CA certificate "
+  default     = true
+}
