fb0b6c70119ce8853eaea3eccb9be39e0f0cce1c
Robert Peteuil <robert@peteuil.com>
2021-08-16 16:27:34 -0700

    Revert "update providers"
    This reverts commit 88e87c60bee7e426a9fc55aa0f6a7b8fc5f9c5a0.

--- versions.tf
+++ versions.tf
@@ -1,12 +1,4 @@
 
 terraform {
   required_version = ">= 0.13"
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-    }
-    tls = {
-      source = "hashicorp/tls"
-    }
-  }
 }
