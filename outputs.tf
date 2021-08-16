12c0cf201c9dbc088c253f8f3694d9fa079c55fb
Robert Peteuil <robert@peteuil.com>
2021-08-16 16:27:44 -0700

    Revert "update for tf 0.13+"
    This reverts commit 4ba0380df2adac67621ecb38effe17a7eb9dde37.

--- outputs.tf
+++ outputs.tf
@@ -1,40 +1,32 @@
 output "ca_key_algorithm" {
-  value     = tls_private_key.root.algorithm
-  sensitive = false
+  value = tls_private_key.root.algorithm
 }
 
 output "ca_private_key_pem" {
-  value     = tls_private_key.root.private_key_pem
-  sensitive = true
+  value = tls_private_key.root.private_key_pem
 }
 
 output "ca_cert_pem" {
-  value     = tls_self_signed_cert.root.cert_pem
-  sensitive = false
+  value = tls_self_signed_cert.root.cert_pem
 }
 
 output "consul_gossip_key" {
-  value     = base64encode(random_id.consul_gossip_key.hex)
-  sensitive = false
+  value = base64encode(random_id.consul_gossip_key.hex)
 }
 
 output "consul_master_token" {
-  value     = random_id.consul_master_token.hex
-  sensitive = false
+  value = random_id.consul_master_token.hex
 }
 
 output "consul_join_tag_value" {
-  value     = random_id.consul_join_tag_value.hex
-  sensitive = false
+  value = random_id.consul_join_tag_value.hex
 }
 
 output "nomad_gossip_key" {
-  value     = base64encode(random_id.nomad_gossip_key.hex)
-  sensitive = false
+  value = base64encode(random_id.nomad_gossip_key.hex)
 }
 
 output "test" {
-  value     = uuid()
-  sensitive = false
+  value = base64encode(random_id.nomad_gossip_key.hex)
 }
 
