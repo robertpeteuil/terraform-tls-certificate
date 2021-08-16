4ba0380df2adac67621ecb38effe17a7eb9dde37
Robert Peteuil <robert@peteuil.com>
2021-08-16 16:14:55 -0700

    update for tf 0.13+

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,40 @@
 output "ca_key_algorithm" {
-  value = tls_private_key.root.algorithm
+  value     = tls_private_key.root.algorithm
+  sensitive = false
 }
 
 output "ca_private_key_pem" {
-  value = tls_private_key.root.private_key_pem
+  value     = tls_private_key.root.private_key_pem
+  sensitive = true
 }
 
 output "ca_cert_pem" {
-  value = tls_self_signed_cert.root.cert_pem
+  value     = tls_self_signed_cert.root.cert_pem
+  sensitive = false
 }
 
 output "consul_gossip_key" {
-  value = base64encode(random_id.consul_gossip_key.hex)
+  value     = base64encode(random_id.consul_gossip_key.hex)
+  sensitive = false
 }
 
 output "consul_master_token" {
-  value = random_id.consul_master_token.hex
+  value     = random_id.consul_master_token.hex
+  sensitive = false
 }
 
 output "consul_join_tag_value" {
-  value = random_id.consul_join_tag_value.hex
+  value     = random_id.consul_join_tag_value.hex
+  sensitive = false
 }
 
 output "nomad_gossip_key" {
-  value = base64encode(random_id.nomad_gossip_key.hex)
+  value     = base64encode(random_id.nomad_gossip_key.hex)
+  sensitive = false
 }
 
 output "test" {
-  value = base64encode(random_id.nomad_gossip_key.hex)
+  value     = uuid()
+  sensitive = false
 }
 
